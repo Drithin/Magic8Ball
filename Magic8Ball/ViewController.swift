c7aebf75bc5e1c05bf02d2375ca6433a20b42d1c
Drithin <drithn@gmail.com>
2017-10-15 00:43:18 -0700

    Coding Challenge of iOS11 & Swift 4 - The Complete iOS App Development Bootcamp

--- Magic8Ball/ViewController.swift
+++ Magic8Ball/ViewController.swift
@@ -9,15 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+     var randomballNum:Int = 0
+    
+    @IBOutlet weak var ballImage: UIImageView!
+    
+    @IBAction func askButton(_ sender: UIButton) {
+        updateballImages()
+    }
+    
+    
+    
+    let ballImageArray = ["ball1", "ball2", "ball3", "ball4", "ball5"]
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        updateballImages()
+    }
+    
+    func updateballImages(){
+        
+        randomballNum = Int(arc4random_uniform(5))
+        
+        ballImage.image = UIImage.init(named: ballImageArray[Int(arc4random_uniform(5))])
+       
+        }
+    
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        updateballImages()
     }
 
 
